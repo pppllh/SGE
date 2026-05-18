c11615a038950f935a957b31f5c80876caf9bdc9
pppllh <164863620+pppllh@users.noreply.github.com>
2026-05-18 18:57:32 +0800

    Add files via upload

---
+++ print('hello').py
@@ -0,0 +1,13 @@
+x =  [('a', 'b', 'c'), ('b', 'd'), ('e', 'f'), ('c', 'g'), ('h', 'i', 'j'), ('f', 'k')]
+def func(x):
+    ans = []
+    for s in x:
+        sett = []
+        for m in ans:
+            if set(s) & m:
+                sett.append(m)
+        new_sett = set(s)
+        ans.append(new_sett)
+    return ans
+print(func(x))
+
